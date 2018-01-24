2e161a8a6dbad873bd680b1be60213e03e398a28
liang <liang.su@beyondz.com.hk>
2018-01-24 20:25:07 +0800

    add script in package.json for run webpack add webpack.config.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learnWebpack
+Using es5 import export works without babel, webpack packaged the 2 files succeed
+<!-- js 3 node module for import and export -->
+<!-- babel is for code transfrom for broweser compability -->
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    }
+
+
+}
+
+module.exports = config;
