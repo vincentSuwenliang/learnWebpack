34fada9692d2233a8a64e2762446c3df217a3c86
liang <liang.su@beyondz.com.hk>
2018-01-25 11:26:08 +0800

    load big image success

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/' //for url-loader(other loaders) will apend 'output.publicPath'
     },
     module: {
         rules: [
